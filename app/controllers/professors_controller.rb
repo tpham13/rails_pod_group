d4e0e93507ebd7b3d3a65f2d710db85a5218fb03
tpham13 <tpham0123@gmail.com>
2020-09-17 15:03:48 -0700

    added index, show, new, create

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,5 +1,20 @@
 class ProfessorsController < ApplicationController
 
   def index
+    @professors = Professor.all
   end
+
+  def show
+    @professor = Professor.find(params[:id])
+  end 
+
+  def new
+    @professor = Professor.new 
+  end 
+
+  def create
+    @professor = Professor.create(params.require(:professor).permit(:name))
+    redirect_to professors_path
+  end 
+
 end
