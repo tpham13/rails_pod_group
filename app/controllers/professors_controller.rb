42cde628f093c2914a6c5ecd813cf6c83b15872c
tpham13 <tpham0123@gmail.com>
2020-09-22 14:53:17 -0700

    full crud added for professors

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,20 +1,37 @@
+
 class ProfessorsController < ApplicationController
 
-  def index
-    @professors = Professor.all
+    def index
+      @professors = Professor.all
+    end
+  
+    def show
+      @professor = Professor.find(params[:id])
+    end 
+  
+    def new
+      @professor = Professor.new 
+    end 
+  
+    def create
+      @professor = Professor.create(params.require(:professor).permit(:name))
+      redirect_to professors_path
+    end 
+
+    def edit
+        @professor = Professor.find(params[:id])
+    end
+
+    def update
+        @professor = Professor.find(params[:id])
+        @professor.update(params.require(:professor).permit(:name))
+        redirect_to professor_path
+    end
+
+    def destroy
+        @professor = Professor.find(params[:id])
+        @professor.destroy
+        redirect_to professors_path
+    end 
+  
   end
-
-  def show
-    @professor = Professor.find(params[:id])
-  end 
-
-  def new
-    @professor = Professor.new 
-  end 
-
-  def create
-    @professor = Professor.create(params.require(:professor).permit(:name))
-    redirect_to professors_path
-  end 
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/professors', to: "professors#index"
-  resources :professors, only: [:index, :new, :create, :edit, :update, :show]
+  resources :professors
+  # patch 'professors' to 'professor#delete'
+
 
 end
